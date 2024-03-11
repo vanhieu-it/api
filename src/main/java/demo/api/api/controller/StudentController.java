c532234258a8abe8fd87ebb257789d03509a99a7
Văn Hiếu <hieu.dovan@thegioididong.com>
2024-03-11 10:49:22 +0700

    up api

---
+++ src/main/java/demo/api/api/controller/StudentController.java
@@ -0,0 +1,33 @@
+package demo.api.api.controller;
+
+import demo.api.api.model.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    private List<Student> students;
+
+    public StudentController() {
+        students = new ArrayList<>();
+        students.add(new Student("1", "Nguyen Van A", "VanA@gmail.com"));
+        students.add(new Student("2", "Nguyen Van B", "VanB@gmail.com"));
+        students.add(new Student("3", "Nguyen Van C", "VanC@gmail.com"));
+    }
+
+    @GetMapping
+    public List<Student> students() {
+        return students;
+    }
+
+    @PostMapping
+    public Student createNewStudent(@RequestBody Student student) {
+        Student newStudent = new Student(student.getId(), student.getFullName(), student.getEmail());
+        students.add(newStudent);
+        return newStudent;
+    }
+
+}
---
+++ src/test/java/demo/api/api/Baitap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baitap1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
